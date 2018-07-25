88572927ffa8c0d0157f0e5da7173652779e2fbc
tom_glb <goodMorning_glb@guigu.com>
2018-07-25 12:51:26 +0800

    hot fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -5,5 +5,8 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("edit");
 		System.out.println("left");
+		System.out.println("right");
+		System.out.println("1111111111111111");
+		System.out.println("hot_fix");
 	}
 }
